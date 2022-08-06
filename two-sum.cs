63ac7b624a8be17b2415d6248626fbb9b81049b8
Sinan Oral <sinnanoral@gmail.com>
2022-08-06 14:43:37 +0300

    Create two-sum.cs

---
+++ two-sum.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for(var i = 0; i < nums.Length; i++)
+        {
+            for(var j = i + 1; j < nums.Length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                    return new[] {i, j};
+            }
+        }
+        
+        return null;
+    }
+}
