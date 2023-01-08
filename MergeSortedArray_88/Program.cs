4152e6bd4d26ef8b4360f2f312318f833ae2a8ad
Sinan Oral <sinnanoral@gmail.com>
2023-01-08 23:53:22 +0300

    add 88-MergeSortedArray

---
+++ MergeSortedArray_88/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace MergeSortedArray_88;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        const int m = 3, n = 3;
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int[] nums2 = { 2, 5, 6 };
+        
+        Merge(nums1, m, nums2, n);
+        
+        foreach (var i in nums1)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("-------------");
+
+        int[] nums3 = { 2, 5, 6, 0, 0, 0 };
+        int[] nums4 = { 1, 2, 3 };
+        
+        Merge2(nums3, m, nums4, n);
+        
+        foreach (var i in nums3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    private static void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int p1 = m - 1, p2 = n - 1;
+        int k = m + n - 1;
+        while (p2 >= 0)
+        {
+            if (p1 >= 0 && nums1[p1] > nums2[p2])
+            {
+                nums1[k] = nums1[p1];
+                p1--;
+                k--;
+            }
+            else
+            {
+                nums1[k] = nums2[p2];
+                p2--;
+                k--;
+            }
+        }
+    }
+    
+    private static void Merge2(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = 0; i < n; i++)
+        {
+            nums1[i + m] = nums2[i];
+        }
+
+        Array.Sort(nums1);
+    }
+}
--- TwoSum_1/Program.cs
+++ TwoSum_1/Program.cs
@@ -5,10 +5,20 @@ internal static class Program
     public static void Main()
     {
         int[] nums1 = { 2, 7, 11, 15 };
-        Console.WriteLine(TwoSum(nums1, 9));
+        var twoSum = TwoSum(nums1, 9);
+        foreach (var i in twoSum)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("--------------------");
 
         int[] nums2 = { 3, 2, 4 };
-        Console.WriteLine(TwoSum2(nums2, 6));
+        var twoSum2 = TwoSum2(nums2, 6);
+        foreach (var i in twoSum2)
+        {
+            Console.WriteLine(i);
+        }
     }
 
     private static int[] TwoSum(int[] nums, int target)
