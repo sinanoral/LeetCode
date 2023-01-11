0b112e889a25c82415c343c314f086d7dcc2660c
Sinan Oral <sinnanoral@gmail.com>
2023-01-11 23:55:47 +0300

    add 74-SearchA2DMatrix

---
+++ PascalsTriangle_118/Program.cs
@@ -0,0 +1,12 @@
+﻿internal static class Program
+{
+    public static void Main()
+    {
+    }
+
+    public static List<List<int>> Generate(int numRows)
+    {
+        var ret = new List<List<int>>(numRows);
+        return ret;
+    }
+}
---
+++ SearchA2DMatrix_74/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SearchA2DMatrix;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        int[][] arr =
+        {
+            new [] { 1, 3, 5, 7 }, 
+            new [] { 10, 11, 16, 20 }, 
+            new [] { 23, 30, 34, 60 }
+        };
+
+        Console.WriteLine(SearchMatrix(arr, 3));
+    }
+
+    private static bool SearchMatrix(int[][] matrix, int target)
+    {
+        if (matrix.Length == 0 || matrix[0].Length == 0)
+            return false;
+
+        int m = matrix.Length;
+        int n = matrix[0].Length;
+
+        int low = 0, high = m * n - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            int midVal = matrix[mid / n][mid % n];
+            if (midVal == target)
+                return true;
+            if (midVal < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return false;
+    }
+}
