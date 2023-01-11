7faee89d7c893c7444aa06ddc4b6c3b92d929263
Sinan Oral <sinnanoral@gmail.com>
2023-01-12 00:58:46 +0300

    add 118-PascalsTriangle

--- PascalsTriangle_118/Program.cs
+++ PascalsTriangle_118/Program.cs
@@ -1,12 +1,28 @@
-﻿internal static class Program
+﻿namespace PascalsTriangle_118;
+
+internal static class Program
 {
     public static void Main()
     {
+        var pascal = Generate(5);
     }
 
-    public static List<List<int>> Generate(int numRows)
+    private static IList<IList<int>> Generate(int numRows)
     {
-        var ret = new List<List<int>>(numRows);
-        return ret;
+        List<IList<int>> pascal= new List<IList<int>>(numRows);
+        for(int i = 0; i< numRows; i++)
+        {
+            var row = new List<int>();
+            for(int j = 0; j <= i; j++)
+            {
+                if((j - 1) < 0 || (j + 1) > i) 
+                    row.Add(1);
+                else  
+                    row.Add(pascal[i - 1][j - 1] + pascal[i - 1][j]);
+            }
+            pascal.Add(row);
+        }
+        
+        return pascal; 
     }
 }
