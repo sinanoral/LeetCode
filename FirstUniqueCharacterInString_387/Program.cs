8aa63aceef5b8fbcf0d45d4c0687e0f7fdac133e
Sinan Oral <sinnanoral@gmail.com>
2023-01-12 23:42:09 +0300

    add 387-FirstUniqueCharacterInString

---
+++ FirstUniqueCharacterInString_387/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FirstUniqueCharacterInString_387;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(FirstUniqChar("leetcode"));
+    }
+    
+    private static int FirstUniqChar(string s) {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        
+        foreach (var c in s)
+        {
+            if (dict.ContainsKey(c))
+                dict[c]++;
+            else
+                dict.Add(c, 1);
+        }
+        
+        foreach (var (key, value) in dict)
+        {
+            if (value == 1)
+            {
+                return s.IndexOf(key);
+            }
+        }
+
+        return -1;
+    }
+}
