8f4a3565d74837c75886902d4f1a5930de90e99c
Sinan Oral <sinnanoral@gmail.com>
2023-01-13 00:21:04 +0300

    add 383-RansomNote

---
+++ RansomNote_383/Program.cs
@@ -0,0 +1,30 @@
+﻿internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(CanConstruct("a", "b"));
+        Console.WriteLine(CanConstruct("aa", "ab"));
+    }
+    
+    private static bool CanConstruct(string ransomNote, string magazine) {
+        Dictionary<char, int> magazineDict = new Dictionary<char, int>();
+
+        foreach(char c in magazine)
+        {
+            if(!magazineDict.ContainsKey(c))
+                magazineDict.Add(c, 1);
+            else
+                magazineDict[c]++;
+        }
+
+        foreach(char c in ransomNote)
+        {
+            if(magazineDict.ContainsKey(c) && magazineDict[c] > 0)
+                magazineDict[c]--;
+            else
+                return false;
+        }
+
+        return true;
+    }
+}
