f01f9312c3efd13dc6e594d13378bcec4a01711c
Sinan Oral <sinnanoral@gmail.com>
2023-01-13 00:27:56 +0300

    add 242-ValidAnagram

---
+++ ValidAnagram_242/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ValidAnagram_242;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(IsAnagram("anagram", "nagaram"));
+        Console.WriteLine(IsAnagram("rat", "car"));
+    }
+    
+    private static bool IsAnagram(string s, string t) {
+
+        if(s.Length != t.Length) return false;
+
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        foreach(char c in s)
+        {
+            if(!dict.ContainsKey(c))
+                dict.Add(c, 1);
+            else
+                dict[c]++;
+        }
+
+        foreach(char c in t)
+        {
+            if(dict.ContainsKey(c) && dict[c] > 0)
+                dict[c]--;
+            else
+                return false;
+        }
+
+        return true;
+    }
+}
