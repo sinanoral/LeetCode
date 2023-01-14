9acf5ce0e64e50dc0852725f7ff9b232197ae71e
Sinan Oral <sinnanoral@gmail.com>
2023-01-14 18:46:35 +0300

    add 141-LinkedListCycle

---
+++ LinkedListCycle_141/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LinkedListCycle_141;
+
+internal class ListNode
+{
+    private int val;
+    public ListNode next;
+
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
+
+internal static class Program {
+    
+    public static void Main(string[] args)
+    {
+        
+    }
+    
+    public static bool HasCycle(ListNode head) {
+
+        if(head is null || head.next is null) return false;
+
+        var set = new HashSet<ListNode>();
+        
+        var temp = head;
+        while(temp.next is not null)
+        {
+            if (set.Contains(temp)) return true;
+            
+            set.Add(temp);
+            temp = temp.next;
+        }
+
+        return false;
+    }
+}
