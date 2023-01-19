7830de48010af65d7ce7df0eec09be69f999b70a
Sinan Oral <sinnanoral@gmail.com>
2023-01-20 00:42:34 +0300

    add 700-Search in a Binary Search Tree

---
+++ SearchInABinarySearchTree_700/Program.cs
@@ -0,0 +1,27 @@
+﻿internal static class Program
+{
+    public static void Main()
+    {
+        
+    }
+    
+    public static TreeNode SearchBST(TreeNode root, int val) {
+        if(root == null || root.val == val) return root;
+
+        return SearchBST(root.val > val ? root.left : root.right, val);
+    }
+}
+
+internal class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
